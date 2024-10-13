1d1012120f68610c641c0857f7610c6f0d4f81d4
HiPhish <hiphish@posteo.de>
2024-10-13 19:31:21 +0200

    Fix formatting of return value samples

--- lua/ansible-doc/format/return.lua
+++ lua/ansible-doc/format/return.lua
@@ -2,24 +2,50 @@ local put = require('ansible-doc.format.util').put
 local appendbufline = vim.fn.appendbufline
 
 
-local KEYS = {'returned', 'sample', 'type'}
+local KEYS = {'returned', 'type'}
 local LABELS = {
 	returned = 'returned',
 	sample = 'sample',
 	type = 'type',
 }
 
+local format_sample
+local formatters = {
+	string = function(value)
+		return string.format('"%s"', value)
+	end,
+	number = function(value)
+		return string.format('%d', value)
+	end,
+	boolean = function(value)
+		return value and 'true' or 'false'
+	end,
+	table = function(value)
+		if vim.islist(value) then
+			local items = vim.tbl_map(format_sample, value)
+			return string.format('[%s]', table.concat(items, ', '))
+		end
+		local items = {}
+		for k, v in pairs(value) do
+			table.insert(items, string.format('%s: %s', format_sample(k), format_sample(v)))
+		end
+		return string.format('{%s}', table.concat(items, ', '))
+	end,
+}
+
+function format_sample(sample)
+	local formatter = formatters[type(sample)]
+	return formatter(sample)
+end
+
 local function put_item(bufnr, name, item)
 	put(bufnr, {'', string.format('  %s', name)})
 	appendbufline(bufnr, '$', string.format('    %s', item.description))
 	vim.cmd 'normal! Ggww'
 	for _, key in ipairs(KEYS) do
-		local value = item[key]
-		if value then
-			-- FIXME: The formatting depends on the `type` field of the item
-			appendbufline(bufnr, '$', string.format('    %s: %s', LABELS[key], value))
-		end
+		appendbufline(bufnr, '$', string.format('    %s: %s', LABELS[key], item[key]))
 	end
+	appendbufline(bufnr, '$', string.format('    %s: %s', LABELS.sample, format_sample(item.sample)))
 end
 
 return function(bufnr, items)
